bedf5d8d3fd91427927682ce76e6e7584230274b
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-02-02 21:52:33 -0500

    Update FirstBank.java

--- FirstBank.java
+++ FirstBank.java
@@ -7,13 +7,16 @@ public class FirstBank {
 	
 	
 	public static void main(String[] args) {
-	//"showMenu funtion gives us access to the other funtions in the Account class
+	//"showMenu function gives us access to the other functions in the Account class
 		
 		Account mark = new Account ("Mark Stevens", "A00001");
 			mark.showMenu();
 	//now that we've created a new instance	of this object named "John", we can call on it.
 		Account john = new Account ("John Marshall", "B00002");
 			john.showMenu();
+		
+		Account sally = new Account ("Sally Cooper", "C00003");
+			sally.showMenu();
 	}
 
 }
